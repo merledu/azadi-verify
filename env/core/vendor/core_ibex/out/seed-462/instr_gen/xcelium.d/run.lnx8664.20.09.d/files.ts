51575ecf07d9a33801093ea4ef04acb78535e3cc
Zain Rizwan Khan <zainrizwankhan@gmail.com>
2021-09-21 01:43:21 +0500

    Deleted out directory from older DV environment

--- env/core/vendor/core_ibex/out/seed-462/instr_gen/xcelium.d/run.lnx8664.20.09.d/files.ts
+++
@@ -1,4 +0,0 @@
-1631247951 /home/zrkhan/Desktop/azadi-new/hw/vendor/lowrisc_ibex/vendor/google_riscv-dv/src/riscv_signature_pkg.sv
-1631247951 /home/zrkhan/Desktop/azadi-new/hw/vendor/lowrisc_ibex/vendor/google_riscv-dv/src/riscv_instr_pkg.sv
-1631247951 /home/zrkhan/Desktop/azadi-new/hw/vendor/lowrisc_ibex/vendor/google_riscv-dv/test/riscv_instr_test_pkg.sv
-1631247951 /home/zrkhan/Desktop/azadi-new/hw/vendor/lowrisc_ibex/vendor/google_riscv-dv/test/riscv_instr_gen_tb_top.sv
--- env/core/vendor/core_ibex/out/seed-462/instr_gen/xcelium.d/run.lnx8664.20.09.d/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1526655675 /opt/cadence/installs/XCELIUM2009/tools.lnx86/methodology/UVM/CDNS-1.2/sv/src/uvm_pkg.sv
-1526655670 /opt/cadence/installs/XCELIUM2009/tools.lnx86/methodology/UVM/CDNS-1.2/additions/sv/cdns_uvm_pkg.sv
