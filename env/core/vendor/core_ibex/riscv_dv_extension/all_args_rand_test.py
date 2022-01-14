bff88e5f0465df8f91c5b4ee6412ecddc24a0724
Auringzaib Sabir <auringzaibsabir11@gmail.com>
2022-01-14 16:16:13 +0500

    Removed unnecessary line from all_args_rand_test.py

--- env/core/vendor/core_ibex/riscv_dv_extension/all_args_rand_test.py
+++ env/core/vendor/core_ibex/riscv_dv_extension/all_args_rand_test.py
@@ -163,7 +163,7 @@ def search_multiple_strings_in_file(file_name, list_of_strings):
                     else:
                     	  print('Comparison FAILED for ',Setting_argument)
 	                  
-	                # If any string is found in line, then append that line along with line number in list
+	            # If any string is found in line, then append that line along with line number in list
                     list_of_results.append((string_to_search, line_number, line.rstrip()))
     # Return list of tuples containing matched string, line numbers and lines where string is found
     return list_of_results
@@ -185,7 +185,6 @@ def copy_in_latest_test():
      now = time.time()
      path = ROOT_DIR+"/out"
      print("path=",path)
-     #directory=os.path.join("/home","/home/asabir/regression_ibex/azadi-verify/env/core/vendor/core_ibex/out")
      directory=os.path.join("/home",path)
      os.chdir(directory)
      for file in os.listdir("."):
@@ -200,7 +199,6 @@ def copy_in_latest_test():
      print ("newest directory is ", latest)
      os.chdir(latest)
      # For copying the all_args_rand_test.yaml in the latest out/seed directory made for specific test
-     #src_path = r"/home/asabir/regression_ibex/azadi-verify/env/core/vendor/core_ibex/riscv_dv_extension/all_args_rand_test.yaml"
      src_path = ROOT_DIR+"/riscv_dv_extension/all_args_rand_test.yaml"
      dst_path = latest + "/all_args_rand_test.yaml"
      print ("src_path = " , src_path)
@@ -208,7 +206,6 @@ def copy_in_latest_test():
      shutil.copy(src_path, dst_path)
      print('Copied "all_args_rand_test.yaml"')
      # For copying the run_test.py in the latest out/seed directory made for specific test
-     #src_path = r"/home/asabir/regression_ibex/azadi-verify/env/core/vendor/core_ibex/riscv_dv_extension/run_test.py"
      src_path = ROOT_DIR+"/riscv_dv_extension/run_test.py"
      dst_path = latest + "/run_test.py"
      print ("src_path = " , src_path)
@@ -224,7 +221,6 @@ def main():
         #value = '    +instr_cnt=' + str(randint(0,10))
         #replace_line('all_args_rand_test.yaml', elem[1],value)
     
-    #test_list = r"/home/asabir/regression_ibex/azadi-verify/env/core/vendor/core_ibex/riscv_dv_extension/all_args_rand_test.yaml"
     test_list = ROOT_DIR+"/riscv_dv_extension/all_args_rand_test.yaml"
     print("testlist=",test_list)
     cmd = "make TEST=all_args_rand_test ITERATIONS=1 "+"TESTLIST="+test_list+" ISA=rv32imfdc COV=1 WAVES=1"
@@ -234,7 +230,6 @@ def main():
     # Changing directory to execute the 'make command'
     print("Printing Current working directory")
     print (os.getcwd())
-    #change_path = "/home/asabir/regression_ibex/azadi-verify/env/core/vendor/core_ibex/"
     change_path = ROOT_DIR+"/"
     os.chdir(change_path)
     print("Changed directory is")
