066978943126e75b8a6bcff1b9d408cfa61c964e
Auringzaib Sabir <auringzaibsabir11@gmail.com>
2022-01-14 15:34:18 +0500

    Incorporated relative paths in run_test.py

--- env/core/vendor/core_ibex/riscv_dv_extension/run_test.py
+++ env/core/vendor/core_ibex/riscv_dv_extension/run_test.py
@@ -29,17 +29,24 @@ from random import randint
 import os
 import sys
 
+# print(__file__)
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+print("ROOT_DIR =", ROOT_DIR)
+
 def main():
     seed = sys.argv
     print ("Seed from the command line = ", seed[1])
-    test_list = "/home/asabir/regression_ibex/azadi-verify/env/core/vendor/core_ibex/out/seed-"+str(seed[1])+"/all_args_rand_test.yaml"
+    #test_list = "/home/asabir/regression_ibex/azadi-verify/env/core/vendor/core_ibex/out/seed-"+str(seed[1])+"/all_args_rand_test.yaml"
+    test_list = ROOT_DIR+"/out/seed-"+str(seed[1])+"/all_args_rand_test.yaml"
+    print("test_list=",test_list)
     cmd = "make TEST=all_args_rand_test ITERATIONS=1 "+"TESTLIST="+test_list+" SEED="+str(seed[1])+" ISA=rv32imfdc COV=1 WAVES=1"
     print("Command to run = ", cmd)
 
     # Changing directory to execute the 'make command'
     print("Printing Current working directory")
     print (os.getcwd())
-    change_path = "/home/asabir/regression_ibex/azadi-verify/env/core/vendor/core_ibex/"
+    #change_path = "/home/asabir/regression_ibex/azadi-verify/env/core/vendor/core_ibex/"
+    change_path = ROOT_DIR+"/"
     os.chdir(change_path)
     print("Changed directory is")
     print (os.getcwd())
