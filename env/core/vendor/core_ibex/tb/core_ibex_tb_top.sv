caf4b28700f560306e7f99491649af65db8b0bcc
Auringzaib Sabir <auringzaibsabir11@gmail.com>
2022-02-16 16:20:42 +0500

    Implemented functional coverage for floating point status flags

--- env/core/vendor/core_ibex/tb/core_ibex_tb_top.sv
+++ env/core/vendor/core_ibex/tb/core_ibex_tb_top.sv
@@ -246,6 +246,8 @@ module core_ibex_tb_top;
       dut.u_ibex_core.cs_registers_i.csr_num_cg_h.sample();
       // Sampling coverage for floating point rounding modes
       dut.u_ibex_core.id_stage_i.decoder_i.fpu_round_mode_cg_h.sample();
+      // Sampling coverage for floating point status flags
+      dut.u_ibex_core.cs_registers_i.fp_status_cg_h.sample();
     `endif
   end
 
